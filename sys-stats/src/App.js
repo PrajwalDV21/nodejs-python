e8d52d2dd9d38caaa6b6ba6148894cae2663b5af
PrajwalDV21 <prajwaldv21@gmail.com>
2023-03-28 21:11:53 +0530

    commiting the change

--- sys-stats/src/App.js
+++ sys-stats/src/App.js
@@ -20,7 +20,8 @@ class App extends Component {
 
   async loadData() {
     try {
-      const res = await fetch('http://localhost:5000/stats');
+      const res = await fetch('http://localhost:3050/api/stats');
+      console.log(res);
       const blocks = await res.json();
       const ram = blocks.ram;
       const cpu = blocks.cpu;
